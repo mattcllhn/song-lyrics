03349cabc20066049141db3e0c14deb33e370d16
matt <mattcllhn@gmail.com>
2016-12-28 15:14:58 -0600

    successfully formatted scraped data

--- server/app.js
+++ server/app.js
@@ -18,27 +18,25 @@ app.get('/',function(req, res){
 
 
 app.get('/scrape',function(req,res){
-var  url = 'http://www.azlyrics.com/lyrics/willienelson/amazinggrace.html';
+// var  url = 'http://www.azlyrics.com/lyrics/willienelson/amazinggrace.html';
+var url = 'http://www.azlyrics.com/lyrics/jeffbuckley/hallelujah.html';
+// var url = 'http://www.azlyrics.com/lyrics/rachelplatten/heyheyhallelujah.html';
 
 request(url, function(error, response, html){
   var objectToSend = {title:"", lyrics:{}};
     if(!error){
-      lyricFormatter('xyz');
         var $ = cheerio.load(html);
         // console.log($);
         var body = $('body').html();
-        console.log('body',body);
+        // console.log('body',body);
           objectToSend.title = $('.ringtone').next().text();
-          // console.log('this is the title',objectToSend.title);
-        objectToSend.lyrics = $('.ringtone').next().next().next().next().html();
-        console.log('lyrics '+ objectToSend.lyrics+' which is typeof '+typeof(objectToSend.lyrics));
-        objectToSend.body = body;
-        // var newArray = objectToSend.lyrics.replace(/ *\[[^\]]*]/g, '');
-        // console.log(newArray);
-        // newArray = newArray.split('"');
-        // console.log(newArray);
-        // var poo = newArray.split('\r');
-        // console.log(newArray);
+          console.log('this is the title',objectToSend.title);
+        objectToSend.lyrics = lyricFormatter($('.ringtone').next().next().next().next().html());
+
+
+
+
+
 
         }//if
         res.send(objectToSend);
--- server/modules/lyricFormatter.js
+++ server/modules/lyricFormatter.js
@@ -1,5 +1,23 @@
 function lyricFormatter(dataIn){
-console.log(dataIn+' in lyricFormatter module, yo');
-  return;
+  var lyrics = dataIn;
+  var arrayToSend;
+//takes out apostrophes
+lyrics = lyrics.replace(/&apos;/g,"'");
+//removes text in square brackets
+lyrics= lyrics.replace(/ *\[[^\]]*]/g, '');
+//removes html tags
+lyrics = lyrics.replace(/<(?:.|\n)*?>/gm, '');
+//removes returns
+lyrics = lyrics.replace(/\r/g, '');
+//formats into an array
+var lyricArray = lyrics.split('\n\n');
+//loop through and create subarrays out of lines
+for (var i = 0; i < lyricArray.length; i++) {
+  lyricArray[i] = lyricArray[i].split('\n');
+  console.log('in the loop bro \n',lyricArray[i]);
+
+
+}//for loop
+  return lyricArray;
 }
 module.exports = lyricFormatter;
