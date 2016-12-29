ded3e75f4c78257011330612c75fe4c4f653b268
matt <mattcllhn@gmail.com>
2016-12-29 15:09:05 -0600

    modified ng-repeat

--- server/modules/domTraverser.js
+++ server/modules/domTraverser.js
@@ -1,9 +1,8 @@
 function domTraverser(dataIn){
           var currentElem = dataIn.next();
           // console.log($('.ringtone').next($('div'))[0].name);
-
         for (var i = 0; i<10; i++) {
-          console.log('currentElem name', currentElem[0].name);
+          // console.log('currentElem name', currentElem[0].name);
           var tag = currentElem[0].name;
           if (tag == 'div') {
             console.log(currentElem.text());
@@ -12,8 +11,8 @@ function domTraverser(dataIn){
           }else{
           currentElem = currentElem.next();
 
-          }
+        }//else
 
-        }
-}
+      }//for loop
+}//domTraverser
 module.exports = domTraverser;
