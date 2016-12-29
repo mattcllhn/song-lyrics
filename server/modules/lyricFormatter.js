f9b9afe15b1ec7118170e3a108382bb564fcd5eb
matt <mattcllhn@gmail.com>
2016-12-29 14:47:37 -0600

    tweaked lyricFormatter

--- server/modules/lyricFormatter.js
+++ server/modules/lyricFormatter.js
@@ -1,8 +1,10 @@
 function lyricFormatter(dataIn){
   var lyrics = dataIn;
   var arrayToSend = [];
-//takes out apostrophes
+//takes out encoded apostrophes
 lyrics = lyrics.replace(/&apos;/g,"'");
+//takes out encoded quotation marks
+lyrics = lyrics.replace(/&quot;/g,' " ');
 //removes text in square brackets
 lyrics= lyrics.replace(/ *\[[^\]]*]/g, '');
 //removes html tags
@@ -15,7 +17,14 @@ var lyricArray = lyrics.split('\n\n');
 for (var i = 0; i < lyricArray.length; i++) {
   if(lyricArray[i].length>1){
   var verse = lyricArray[i].split('\n');
-  arrayToSend.push(verse);
+//clears out empty lines in verses
+  var verseToSend = [];
+  verse.map(function(int){
+    if(int!==''){
+      verseToSend.push(int);
+    }
+  });//map method
+  arrayToSend.push(verseToSend);
   console.log('in the loop bro \n',lyricArray[i]);
 }
 
