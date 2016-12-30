b877e114957117d981f88e115aa22eb3cc04adfe
matt <mattcllhn@gmail.com>
2016-12-30 13:50:55 -0600

    lyric duplicates removed client side

--- public/scripts.js
+++ public/scripts.js
@@ -10,30 +10,18 @@ var myApp = angular.module('myApp',[]);
     url:"/scrape"
   }).then(function(res){
     console.log("res back",res.data);
-    var formattedResponse = duplicateRemover(res.data.lyrics);
-    console.log('formattedResponse', formattedResponse);
+
+
     $scope.song = res.data;
-    // console.log('scope.song',$scope.song);
-  });//
-    };
+    $scope.lyrics = duplicateRemover(res.data.lyrics);
+  });//.then function
+};//$scope.scrape
   }]);//scrapecontroller
 
 
   function duplicateRemover(lyrics){
-    var response = [];
-    var loopArray = lyrics;
-    // console.log(loopArray);
-    //loops through  verse blocks
-    for (var i = 0; i < loopArray.length; i++) {
-      for (var jj = 0; jj < loopArray.length; jj++) {
-        if(loopArray[i][0]==loopArray[jj][0]){
-          console.log(loopArray[i]+' \n<\nMATCHES\n>\n '+loopArray[jj]);
-        }
-      }
-      // if(loopArray[i]!==loopArray[i+1]){
-      //   console.log(loopArray[i][0]+' does not equal '+loopArray[i+1][0]);
-      //   response.push(loopArray[i]);
-      // }
-    }//for loop
-    return response;
+    var seen = {};
+      return lyrics.filter(function(item) {
+      return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+      });
   }//duplicateRemover
