2869d75e391f8b379f2e9c507d4f0e8095b46996
matt <mattcllhn@gmail.com>
2016-12-30 13:05:28 -0600

    array looping client side

--- public/scripts.js
+++ public/scripts.js
@@ -8,10 +8,32 @@ var myApp = angular.module('myApp',[]);
   $http({
     method:"GET",
     url:"/scrape"
-  }).then(function(data){
-    console.log("data back",data.data);
-    $scope.song = data.data;
+  }).then(function(res){
+    console.log("res back",res.data);
+    var formattedResponse = duplicateRemover(res.data.lyrics);
+    console.log('formattedResponse', formattedResponse);
+    $scope.song = res.data;
     // console.log('scope.song',$scope.song);
   });//
     };
   }]);//scrapecontroller
+
+
+  function duplicateRemover(lyrics){
+    var response = [];
+    var loopArray = lyrics;
+    // console.log(loopArray);
+    //loops through  verse blocks
+    for (var i = 0; i < loopArray.length; i++) {
+      for (var jj = 0; jj < loopArray.length; jj++) {
+        if(loopArray[i][0]==loopArray[jj][0]){
+          console.log(loopArray[i]+' \n<\nMATCHES\n>\n '+loopArray[jj]);
+        }
+      }
+      // if(loopArray[i]!==loopArray[i+1]){
+      //   console.log(loopArray[i][0]+' does not equal '+loopArray[i+1][0]);
+      //   response.push(loopArray[i]);
+      // }
+    }//for loop
+    return response;
+  }//duplicateRemover
